73341b3da2d5b6b8ce3412059190d948ec3f6687
raigo132 <raigo.kovask@gmail.com>
2023-11-24 15:58:40 +0200

    feat: update avatar created event

--- src/events/avatar-exported.event.ts
+++ src/events/avatar-exported.event.ts
@@ -2,6 +2,8 @@ import { IFrameEvent } from '../types';
 
 export type AvatarExportedEventPayload = {
   url: string;
+  userId: string;
+  avatarId: string;
 };
 
 export type AvatarExportedEvent = IFrameEvent<AvatarExportedEventPayload>;
